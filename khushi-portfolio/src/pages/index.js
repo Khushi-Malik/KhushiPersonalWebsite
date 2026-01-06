df14cced63acfd99b5d69522a695723ddf99413c
Khushi-Malik <khushimalik1910@gmail.com>
2026-01-05 19:22:55 -0500

    forgot to import blogpost

--- khushi-portfolio/src/pages/index.js
+++ khushi-portfolio/src/pages/index.js
@@ -4,5 +4,6 @@ import Projects from './Projects'
 import Contact from './Contact'
 import Blog from './Blog'
 import Achievement from './Achievement'
+import BlogPost from './BlogPost'
 
-export { Home, About, Projects, Contact, Blog, Achievement };
+export { Home, About, Projects, Contact, Blog, Achievement, BlogPost };
